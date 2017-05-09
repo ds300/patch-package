66391b77bf15590820948772ab242ea1c74339d6
David Sheldrick <david@futurice.com>
2017-05-09 11:35:40 +0100

    make --help option for cli

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { bold, cyan, italic } from "chalk"
 import { argv } from "process"
 import applyPatches from "./applyPatches"
 import getAppRootPath from "./getAppRootPath"
@@ -8,7 +9,35 @@ if (argv.length === 2) {
   applyPatches(appPath)
 } else {
   const packageNames = [].slice.call(argv, 2)
-  packageNames.forEach((packageName: string) => {
-    makePatch(packageName, appPath)
-  })
+  if (packageNames.indexOf("--help") > -1 || packageNames.indexOf("-h") > -1) {
+    printHelp()
+  } else {
+    packageNames.forEach((packageName: string) => {
+      makePatch(packageName, appPath)
+    })
+  }
+}
+
+function printHelp() {
+  console.log(`
+Usage:
+
+    ${bold("patch-package")}${italic("[ <package-name>]")}
+
+
+  Without arguments, the ${bold("patch-package")} command will attempt to find and apply
+  patch files to your project. It looks for files named like
+
+     ./patches/<package-name>:<version>.patch
+
+  When given package names as arguments, patch-package will create patch files
+  based on any changes you've made to the version installed by yarn/npm.
+
+  Add the following to your package.json to ensure that these patches are
+  gracefully applied whenever yarn or npm make changes to node_modules:
+
+    "scripts": {
+      "prepare": "patch-package"
+    }
+`)
 }

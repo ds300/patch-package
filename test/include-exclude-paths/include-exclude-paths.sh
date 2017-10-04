60ab4dad20a2c7520c88fe9bc220b4af939833d2
David Sheldrick <david@futurice.com>
2017-10-04 09:21:07 +0100

    rewrite filtering tests to make snapshots smaller

--- test/include-exclude-paths/include-exclude-paths.sh
+++ test/include-exclude-paths/include-exclude-paths.sh
@@ -5,20 +5,20 @@ echo "add patch-package"
 yarn add $1
 
 echo "edit some files"
-sed -i '' -e 's/baseClamp/patchPackage/g' node_modules/lodash/_baseClamp.js
-sed -i '' -e 's/baseClone/patchPackage/g' node_modules/lodash/_baseClone.js
-sed -i '' -e 's/flip/flop/g' node_modules/lodash/flip.js
+sed -i '' -e 's/exports/patchPackage/g' node_modules/lodash/_baseClamp.js
+sed -i '' -e 's/exports/patchPackage/g' node_modules/lodash/_baseClone.js
+sed -i '' -e 's/exports/patchPackage/g' node_modules/lodash/flip.js
 
 echo "add a file"
 echo "this is a new file" > node_modules/lodash/newFile.md
 
 echo "remove a file"
-rm node_modules/lodash/xor.js
+rm node_modules/lodash/fp/__.js
 
-echo "run patch-package with only xor included"
-patch-package lodash --include xor
+echo "run patch-package with only __.js included"
+patch-package lodash --include __
 
-echo "SNAPSHOT: only xor being deleted"
+echo "SNAPSHOT: only __.js being deleted"
 cat patches/lodash*
 echo "END SNAPSHOT"
 

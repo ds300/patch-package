ad102b217708f987e441ffe61a5ab23cc5207f57
David Sheldrick <david@futurice.com>
2017-10-31 19:19:18 +0000

    add test for adding and deleting files

--- src/__tests__/shellTests.ts
+++ src/__tests__/shellTests.ts
@@ -4,7 +4,10 @@ import * as path from "../path"
 import * as tmp from "tmp"
 import spawnSync from "../spawnSafe"
 
-export function runShellTest(projectName: string) {
+export function runShellTest(
+  projectName: string,
+  shouldProduceSnapshots: boolean = true,
+) {
   describe(`Test ${projectName}:`, () => {
     const tmpDir = tmp.dirSync({ unsafeCleanup: true })
     fs.copySync(path.join("test", projectName), tmpDir.name, {
@@ -17,26 +20,35 @@ export function runShellTest(projectName: string) {
 
     const output = result.stdout.toString() + "\n" + result.stderr.toString()
 
+    it("should exit with 0 status", () => {
+      expect(result.status).toBe(0)
+    })
+
     it("should produce output", () => {
       expect(output.trim()).toBeTruthy()
     })
 
     const snapshots = output.match(/SNAPSHOT: ?([\s\S]*?)END SNAPSHOT/g)
 
-    it("should produce some snapshots", () => {
-      expect(snapshots && snapshots.length).toBeTruthy()
-    })
-
-    if (snapshots) {
-      snapshots.forEach(snapshot => {
-        const snapshotDescriptionMatch = snapshot.match(/SNAPSHOT: (.*)/)
-        if (snapshotDescriptionMatch) {
-          it(snapshotDescriptionMatch[1], () => {
-            expect(snapshot).toMatchSnapshot()
-          })
-        } else {
-          throw new Error("bad snapshot format")
-        }
+    if (shouldProduceSnapshots) {
+      it("should produce some snapshots", () => {
+        expect(snapshots && snapshots.length).toBeTruthy()
+      })
+      if (snapshots) {
+        snapshots.forEach(snapshot => {
+          const snapshotDescriptionMatch = snapshot.match(/SNAPSHOT: (.*)/)
+          if (snapshotDescriptionMatch) {
+            it(snapshotDescriptionMatch[1], () => {
+              expect(snapshot).toMatchSnapshot()
+            })
+          } else {
+            throw new Error("bad snapshot format")
+          }
+        })
+      }
+    } else {
+      it("should not produce any snapshots", () => {
+        expect(snapshots && snapshots.length).toBeFalsy()
       })
     }
   })
---
+++ test/adding-and-deleting-files/adding-and-deleting-files.sh
@@ -0,0 +1,30 @@
+# make sure errors stop the script
+set -e
+
+echo "add patch-package"
+yarn add $1
+
+echo "add a file"
+echo "this is a new file" > node_modules/lodash/newFile.md
+
+echo "remove a file"
+rm node_modules/lodash/fp/__.js
+
+echo "generate patch file"
+patch-package lodash
+
+echo "remove node_modules"
+rm -rf node_modules
+
+echo "resintall and patch node_modules"
+yarn
+yarn patch-package
+
+echo "check that the file was added"
+ls node_modules/lodash/newFile.md
+
+echo "check that the file was removed"
+if ls node_modules/lodash/fp/__.js
+then
+  exit 1
+fi

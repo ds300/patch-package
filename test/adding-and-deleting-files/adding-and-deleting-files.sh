ede98006aa696d04e15b8c03de193dd822af5526
David Sheldrick <david@futurice.com>
2017-11-01 00:50:36 +0000

    more sanity checks

--- test/adding-and-deleting-files/adding-and-deleting-files.sh
+++ test/adding-and-deleting-files/adding-and-deleting-files.sh
@@ -4,6 +4,15 @@ set -e
 echo "add patch-package"
 yarn add $1
 
+echo "check that no new file exists to begin with"
+if ls node_modules/lodash/newFile.md
+then
+  exit 1
+fi
+
+echo "check that the file to be removed is there"
+ls node_modules/lodash/fp/__.js
+
 echo "add a file"
 echo "this is a new file" > node_modules/lodash/newFile.md
 

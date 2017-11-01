c93f59ef60e4a3a5b3d5eec814756bf4214ce5dc
David Sheldrick <david@futurice.com>
2017-11-01 00:33:22 +0000

    uncomment more of the test file

--- test/adding-and-deleting-files/adding-and-deleting-files.sh
+++ test/adding-and-deleting-files/adding-and-deleting-files.sh
@@ -13,18 +13,18 @@ rm node_modules/lodash/fp/__.js
 echo "generate patch file"
 patch-package lodash
 
-# echo "remove node_modules"
-# rm -rf node_modules
+echo "remove node_modules"
+rm -rf node_modules
 
-# echo "resintall and patch node_modules"
-# yarn
-# yarn patch-package
+echo "resintall and patch node_modules"
+yarn
+yarn patch-package
 
-# echo "check that the file was added"
-# ls node_modules/lodash/newFile.md
+echo "check that the file was added"
+ls node_modules/lodash/newFile.md
 
-# echo "check that the file was removed"
-# if ls node_modules/lodash/fp/__.js
-# then
-#   exit 1
-# fi
+echo "check that the file was removed"
+if ls node_modules/lodash/fp/__.js
+then
+  exit 1
+fi

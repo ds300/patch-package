7fe330e3996ec26a50e93ff51f940ba2fe823abd
David Sheldrick <david@futurice.com>
2017-11-01 00:18:57 +0000

    only run one test on travis

--- test/adding-and-deleting-files/adding-and-deleting-files.sh
+++ test/adding-and-deleting-files/adding-and-deleting-files.sh
@@ -4,27 +4,27 @@ set -e
 echo "add patch-package"
 yarn add $1
 
-echo "add a file"
-echo "this is a new file" > node_modules/lodash/newFile.md
+# echo "add a file"
+# echo "this is a new file" > node_modules/lodash/newFile.md
 
-echo "remove a file"
-rm node_modules/lodash/fp/__.js
+# echo "remove a file"
+# rm node_modules/lodash/fp/__.js
 
-echo "generate patch file"
-patch-package lodash
+# echo "generate patch file"
+# patch-package lodash
 
-echo "remove node_modules"
-rm -rf node_modules
+# echo "remove node_modules"
+# rm -rf node_modules
 
-echo "resintall and patch node_modules"
-yarn
-yarn patch-package
+# echo "resintall and patch node_modules"
+# yarn
+# yarn patch-package
 
-echo "check that the file was added"
-ls node_modules/lodash/newFile.md
+# echo "check that the file was added"
+# ls node_modules/lodash/newFile.md
 
-echo "check that the file was removed"
-if ls node_modules/lodash/fp/__.js
-then
-  exit 1
-fi
+# echo "check that the file was removed"
+# if ls node_modules/lodash/fp/__.js
+# then
+#   exit 1
+# fi

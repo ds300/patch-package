1a648d8dc44436d876ad3e1e5fa802c6bdacf243
David Sheldrick <david@futurice.com>
2017-11-01 00:29:32 +0000

    uncomment more of the test file

--- test/adding-and-deleting-files/adding-and-deleting-files.sh
+++ test/adding-and-deleting-files/adding-and-deleting-files.sh
@@ -4,14 +4,14 @@ set -e
 echo "add patch-package"
 yarn add $1
 
-# echo "add a file"
-# echo "this is a new file" > node_modules/lodash/newFile.md
+echo "add a file"
+echo "this is a new file" > node_modules/lodash/newFile.md
 
-# echo "remove a file"
-# rm node_modules/lodash/fp/__.js
+echo "remove a file"
+rm node_modules/lodash/fp/__.js
 
-# echo "generate patch file"
-# patch-package lodash
+echo "generate patch file"
+patch-package lodash
 
 # echo "remove node_modules"
 # rm -rf node_modules

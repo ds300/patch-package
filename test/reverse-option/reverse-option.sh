7bd5cb854cbb3cd5ac0ffdce420e4173106d1a26
David Sheldrick <david@futurice.com>
2018-02-10 11:23:02 +0200

    add failing tests for idempotency

--- test/reverse-option/reverse-option.sh
+++ test/reverse-option/reverse-option.sh
@@ -42,10 +42,36 @@ then
 fi
 grep patchPackage node_modules/lodash/_baseClamp.js
 
+echo "apply the patch again to make sure it's an idempotent operation"
+yarn patch-package
+
+echo "make sure the patch is still applied"
+ls node_modules/lodash/newFile.md
+if node_modules/lodash/fp/__.js
+then
+  exit 1
+fi
+grep patchPackage node_modules/lodash/_baseClamp.js
+
+
 echo "unapply the patch"
 yarn patch-package --reverse
 
 echo "make sure the patch is unapplied"
+if ls node_modules/lodash/newFile.md
+then
+  exit 1
+fi
+if grep patchPackage node_modules/lodash/_baseClamp.js
+then
+  exit 1
+fi
+ls node_modules/lodash/fp/__.js
+
+echo "unapply the patch again to make sure it's an idempotent operation"
+yarn patch-package --reverse
+
+echo "make sure the patch is still unapplied"
 if ls node_modules/lodash/newFile.md
 then
   exit 1

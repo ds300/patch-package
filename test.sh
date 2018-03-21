1fbbeeea2fde997c9fb22eb5e7633bc8bc052eb7
David Sheldrick <david@futurice.com>
2018-03-21 21:08:16 +0000

    remove test.sh. Whoops.

--- test.sh
+++
@@ -1,5 +0,0 @@
-new_version=5.4.5
-
-echo "Go here, make the PR, and get it merged:"
-echo ""
-echo "    https://appgithub.moneycorp.com/Moneycorp/wabelane/compare/master...version-bump/v$new_version?expand=1"

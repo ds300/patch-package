7844ef99dbfe471586bdfb7455eff7a86fadd252
David Sheldrick <david@futurice.com>
2018-03-22 08:17:24 +0000

    reversing patches

--- src/patch.ts
+++ src/patch.ts
@@ -1,27 +1,72 @@
 import * as fs from "fs-extra"
 
 export function parseHunkHeaderLine(headerLine: string) {
-  const match = headerLine.trim().match(/^@@ -(\d+),(\d+) \+(\d+),(\d+) @@$/)
+  const match = headerLine
+    .trim()
+    .match(/^@@ -(\d+)(,(\d+))? \+(\d+)(,(\d+))? @@.*/)
   if (!match) {
     throw new Error(`Bad header line: '${headerLine}'`)
   }
 
   return {
     original: {
-      length: Number(match[2]),
+      length: Number(match[3] || 0),
       start: Number(match[1]),
     },
     patched: {
-      length: Number(match[4]),
-      start: Number(match[3]),
+      length: Number(match[6] || 0),
+      start: Number(match[4]),
     },
   }
 }
 
-export function patch(patchFilePath: string /*, reverse: boolean */) {
+const trimRight = (s: string) => s.replace(/\s+$/, "")
+
+function invertPatchFileLines(lines: string[]) {
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+    if (line.startsWith("--- ")) {
+      // switch over startPath and endPath
+      const other = lines[i + 1]
+      if (line.startsWith("--- /dev/null")) {
+        lines[i + 1] = "+++ /dev/null"
+        lines[i] = "--- a/" + other.slice("+++ b/".length)
+      } else {
+        lines[i + 1] = "+++ b/" + line.slice("--- a/".length)
+        if (other.startsWith("+++ /dev/null")) {
+          lines[i] = "--- /dev/null"
+        } else {
+          lines[i] = "--- a/" + other.slice("+++ b/".length)
+        }
+      }
+      i++
+    } else if (line.startsWith("rename from ")) {
+      const fromName = line.slice("rename from ".length)
+      const toName = line.slice("rename to ".length)
+      lines[i] = "rename from " + toName
+      lines[++i] = "rename to " + fromName
+    } else if (line.startsWith("@@ ")) {
+      const { original, patched } = parseHunkHeaderLine(line)
+      lines[
+        i
+      ] = `@@ -${patched.start},${patched.length} +${original.start},${original.length} @@`
+    } else if (line.startsWith("-")) {
+      lines[i] = "+" + line.slice(1)
+    } else if (line.startsWith("+")) {
+      lines[i] = "-" + line.slice(1)
+    }
+  }
+}
+
+export function patch(patchFilePath: string, reverse: boolean = false) {
   const patchFileContents = fs.readFileSync(patchFilePath).toString()
+  // invert patch file here first
 
   const patchFileLines = patchFileContents.split(/\r?\n/)
+
+  if (reverse) {
+    invertPatchFileLines(patchFileLines)
+  }
 
   const moves: Record<string, string> = {}
 
@@ -36,11 +81,7 @@ export function patch(patchFilePath: string /*, reverse: boolean */) {
       const startPath = line.slice("rename from ".length)
       const endPath = patchFileLines[i++].slice("rename to ".length).trim()
       moves[startPath] = endPath
-      // if (reverse) {
-      //   fs.moveSync(endPath, startPath)
-      // } else {
       fs.moveSync(startPath, endPath)
-      // }
       continue
     }
 
@@ -53,6 +94,11 @@ export function patch(patchFilePath: string /*, reverse: boolean */) {
       // slice of the 'a/'
       fs.unlinkSync(startPath.slice(2))
       // ignore hunk header
+      parseHunkHeaderLine(patchFileLines[i++])
+      // ignore all -lines
+      while (i < patchFileLines.length && patchFileLines[i].startsWith("-")) {
+        i++
+      }
     } else if (startPath === "/dev/null") {
       // creating a new file
       // just grab all the contents and put it in the file
@@ -96,8 +142,10 @@ export function patch(patchFilePath: string /*, reverse: boolean */) {
             // context line
             // check it's the same as in the file
             if (
-              patchFileLines[i++].slice(1) !==
-              fileLines[hunkHeader.original.start - 1 + contextIndex++]
+              trimRight(patchFileLines[i++].slice(1)) !==
+              trimRight(
+                fileLines[hunkHeader.original.start - 1 + contextIndex++],
+              )
             ) {
               console.log(fileLines)
               throw new Error(
@@ -116,8 +164,8 @@ export function patch(patchFilePath: string /*, reverse: boolean */) {
             // delete the line
             // check it's the same as in the file
             if (
-              patchFileLines[i++].slice(1) !==
-              fileLines[hunkHeader.original.start - 1 + contextIndex]
+              trimRight(patchFileLines[i++].slice(1)) !==
+              trimRight(fileLines[hunkHeader.original.start - 1 + contextIndex])
             ) {
               throw new Error(
                 `mismatched lines '${patchFileLines[i - 1].slice(

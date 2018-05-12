0486f496056aa142a80dbab2b75bcb3eb58b52b3
David Sheldrick <david@futurice.com>
2018-05-12 23:24:31 +0100

    fix package-gets-updated test

--- integration-tests/package-gets-updated/package-gets-updated.sh
+++ integration-tests/package-gets-updated/package-gets-updated.sh
@@ -8,19 +8,19 @@ echo "SNAPSHOT: left-pad should contain patch-package"
 grep patch-package node_modules/left-pad/index.js
 echo "END SNAPSHOT"
 
-echo "SNAPSHOT: warning when the patch was applied but version changed"
+(>&2 echo "SNAPSHOT: warning when the patch was applied but version changed")
 yarn add left-pad@1.1.2
-echo "END SNAPSHOT"
+(>&2 echo "END SNAPSHOT")
 
 echo "SNAPSHOT: left-pad should still contain patch-package"
 grep patch-package node_modules/left-pad/index.js
 echo "END SNAPSHOT"
 
-echo "SNAPSHOT: fail when the patch was not applied"
+(>&2 echo "SNAPSHOT: fail when the patch was not applied")
 if yarn add left-pad@1.1.3 ; then
   exit 1
 fi
-echo "END SNAPSHOT"
+(>&2 echo "END SNAPSHOT")
 
 echo "left-pad should not contain patch-package"
 if grep patch-package node_modules/left-pad/index.js ; then

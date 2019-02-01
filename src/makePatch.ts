abb8ec80586c8f7e7461879beb030a78b35aca4d
Dustin Falgout <dustin@falgout.us>
2019-01-31 20:24:02 -0600

    Pass patchesDir through path.resolve()
    This allows the use of a relative path for `patchDir`.

--- src/makePatch.ts
+++ src/makePatch.ts
@@ -87,7 +87,7 @@ export const makePatch = (
   const tmpRepoPackageJsonPath = join(tmpRepoNpmRoot, "package.json")
 
   try {
-    const patchesDir = join(appPath, patchDir)
+    const patchesDir = path.resolve(join(appPath, patchDir))
 
     console.info(grey("•"), "Creating temporary folder")
 

c160644a03418a4393d5b296da39a4372dd19cf5
David Sheldrick <djsheldrick@gmail.com>
2019-02-24 17:08:56 +0000

    add test for --patch-dir 😬

---
+++ integration-tests/custom-patch-dir/custom-patch-dir.sh
@@ -0,0 +1,24 @@
+# make sure errors stop the script
+set -e
+
+echo "add patch-package"
+yarn add $1
+
+echo "modify left-pad"
+npx replace pad patch-package node_modules/left-pad/index.js
+
+mkdir my
+
+echo "make patch file"
+patch-package left-pad --patch-dir my/patches
+
+ls my/patches/left-pad*
+
+echo "reinstall node_modules"
+rimraf node_modules
+yarn
+
+echo "run patch-package"
+patch-package --patch-dir my/patches
+
+grep patch-package node_modules/left-pad/index.js

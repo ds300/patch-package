44f71a0c89e50165f8d437fffd4dfae8bda159d1
David Sheldrick <djsheldrick@gmail.com>
2019-02-25 11:46:24 +0000

    fix custom-resolutions test

--- integration-tests/custom-resolutions/custom-resolutions.sh
+++ integration-tests/custom-resolutions/custom-resolutions.sh
@@ -5,7 +5,7 @@ echo "add patch-package"
 yarn add $1
 
 echo "make changes to dependency/index.js"
-npx replace dependency patch-package node_modules/dependency/index.js
+echo '// hello i am patch-package' > node_modules/dependency/index.js
 
 echo "doesn't fail when making a patch"
 npx patch-package dependency

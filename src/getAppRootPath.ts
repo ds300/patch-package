30087123a7e029b97a3f88a86f36638c4c253b38
David Sheldrick <d.j.sheldrick@gmail.com>
2019-10-30 14:26:09 +0100

    avoid infinite loop when invoked sans package.json

--- src/getAppRootPath.ts
+++ src/getAppRootPath.ts
@@ -5,7 +5,11 @@ import { existsSync } from "fs-extra"
 export const getAppRootPath = (): string => {
   let cwd = process.cwd()
   while (!existsSync(join(cwd, "package.json"))) {
-    cwd = resolve(cwd, "../")
+    const up = resolve(cwd, "../")
+    if (up === cwd) {
+      throw new Error("no package.json found for this project")
+    }
+    cwd = up
   }
   return cwd
 }

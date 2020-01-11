f9b803cab1bddb559785409165852ab33431d190
Christian Bundy <christianbundy@fraction.io>
2020-01-11 08:10:23 -0800

    Remove update notifier

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 #! /usr/bin/env node
 
-const updateNotifier = require("update-notifier")
-const pkg = require("./package.json")
-
-const isCi = require("is-ci")
-
-if (!isCi) {
-  updateNotifier({ pkg }).notify({ isGlobal: false })
-}
-
 require("./dist/index.js")

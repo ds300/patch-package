fda7d2448163a62df25a335d5d2d013a6d38904c
Peter Abbondanzo <pabbondanzo@hubspot.com>
2022-04-14 17:12:47 -0400

    Navigate to this working directory

--- integration-tests/ignores-scripts-when-making-patch/naughty-package/postinstall.sh
+++ integration-tests/ignores-scripts-when-making-patch/naughty-package/postinstall.sh
@@ -1,8 +1,8 @@
 # postinstall
+cd "$(dirname "$0")"
 
 # if we're running in the main repo is ok
-if ls ../patch-package ;
-then
+if ls ../patch-package; then
   exit 0
 fi
 # if we are installed alone then fail
